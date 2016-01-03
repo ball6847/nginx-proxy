9847fc5d84e92ba573e3c3f69082500afde8890c
Porawit Poboonma <ball6847@gmail.com>
2016-01-03 22:15:03 +0700

    use docker event to trigger generate.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
-#!/bin/bash
+#!/bin/sh
+
+start_listener() {
+  docker events --filter 'event=start' | while read event
+  do
+    /generate.sh
+  done
+}
 
 # run generate script
-. /generate.sh
+/generate.sh
+
+start_listener&
 
 # start nginx service
 nginx -g "daemon off;"
